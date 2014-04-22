8f99b48c5daa12e2345f188d779c7ebcc7a385b1
ZhangHao <zhlookin@gmail.com>
2014-04-23 01:07:46 +0800

    add IOTest.py for further study

---
+++ IOTest.py
@@ -0,0 +1,14 @@
+name = raw_input('Please input your name: ')
+print "Hello " + name
+# This is a single line comment
+a = 100
+if a > 0:
+	print a
+else
+	print -a
+
+'''
+This is a multiline
+comment
+'''
+# TODO: Learn data type and struct and string in Python
